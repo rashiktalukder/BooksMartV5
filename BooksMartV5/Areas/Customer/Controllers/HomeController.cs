95c8c0dce5c63f2182eb968dc38632a859f10cf0
Kawsar Rashik <kawsar.rashik@bizzntek.com>
2022-09-19 21:52:38 +0600

    Configuring ViewStart and Imports

--- BooksMartV5/Areas/Customer/Controllers/HomeController.cs
+++ BooksMartV5/Areas/Customer/Controllers/HomeController.cs
@@ -8,8 +8,9 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace BooksMartV5.Controllers
+namespace BooksMartV5.Areas.Customer.Controllers
 {
+    [Area("Customer")]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
